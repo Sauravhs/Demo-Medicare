d252083fddbe06407a2edf9d719d30583319e68d
Saurav Sinha <sauravkrsh7@gmail.com>
2025-12-17 07:08:05 +0530

    Initial commit

---
+++ backend-nest/src/auth/auth.service.ts
@@ -0,0 +1,59 @@
+ import {
+  Injectable,
+  UnauthorizedException,
+  ConflictException,
+} from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import { RegisterDto } from './dto/registerUser.dto';
+import { LoginDto } from './dto/login.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async registerUser(registerUserDto: RegisterDto) {
+    const existingUser = await this.userService.findByEmail(
+      registerUserDto.email,
+    );
+
+    if (existingUser) {
+      throw new ConflictException('User already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(registerUserDto.password, 10);
+
+    const user = await this.userService.createUser({
+      ...registerUserDto,
+      password: hashedPassword,
+    });
+
+    const payload = { sub: user._id, email: user.email };
+    const access_token = await this.jwtService.signAsync(payload);
+
+    return { access_token };
+  }
+
+  async loginUser(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+
+    const user = await this.userService.findByEmail(email);
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const payload = { sub: user._id, email: user.email };
+    const access_token = await this.jwtService.signAsync(payload);
+
+    return { access_token };
+  }
+}
---
+++ backend-nest/src/user/user.service.ts
@@ -0,0 +1,41 @@
+ import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { RegisterDto } from 'src/auth/dto/registerUser.dto';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<User>,
+  ) {}
+
+  async createUser(registerUserDto: RegisterDto) {
+    try {
+      return await this.userModel.create({
+        fname: registerUserDto.fname,
+        lname: registerUserDto.lname,
+        email: registerUserDto.email,
+        password: registerUserDto.password,
+      });
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new ConflictException('Email is already in use');
+      }
+      throw error;
+    }
+  }
+
+  async findByEmail(email: string) {
+    return this.userModel.findOne({ email });
+  }
+
+  // user.service.ts
+   async getUserById(id: string) {
+    return await this.userModel.findOne({ _id: id });
+  }
+ 
+
+
+}
